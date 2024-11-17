b3615ee2c0a009510bcd84fbc3ad36a15f7e1f9f
Bobrovsky Nikita <bobrovskii.nikita1471@mail.ru>
2024-11-17 21:23:01 +0300

    fix format date

--- src/components/userItem/userItem.js
+++ src/components/userItem/userItem.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './userItem.css';
-import cancel from '../../assets/icons/cancel.svg'
+import cancel from '../../assets/icons/cancel.svg';
 
-const UserItem = ({user, onDelete}) => {
+const UserItem = ({ user, onDelete }) => {
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = String(date.getFullYear()).slice(-2);
+
+        return `${day}.${month}.${year}`;
+    };
+
     return (
         <div className="userItem">
             <span className="spanUsername">{user.username}</span>
             <span>{user.email}</span>
-            <span>{user['registration_date']}</span>
+            <span>{formatDate(user.registration_date)}</span> {/* Используем форматированную дату */}
             <span>{user.rating}</span>
             <button className="deleteButton" onClick={() => onDelete(user.id)}>
-                <img src={cancel} alt="Удалить" className="deleteIcon"/>
+                <img src={cancel} alt="Удалить" className="deleteIcon" />
             </button>
         </div>
     );
